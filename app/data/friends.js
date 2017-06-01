ffbd8ebee2e2259703d6938a2620a3768a51d0b3
Allen Chen <allenchen@Allens-MacBook-Pro.local>
2017-06-01 17:39:47 -0400

    completed app

---
+++ app/data/friends.js
@@ -0,0 +1,37 @@
+var friends = [
+{
+	name: "jessica",
+	photo: "https://scontent-lga3-1.xx.fbcdn.net/v/t1.0-9/11796427_10153465531721678_3956520224721410264_n.jpg?oh=2929b0e3b2c2a1ca5772453f5e056758&oe=59A362E4",
+	scores: [
+	"1",
+	"1",
+	"1",
+	"1"
+	]
+},
+{
+	name: "Allen",
+	photo: "https://3.bp.blogspot.com/-W__wiaHUjwI/Vt3Grd8df0I/AAAAAAAAA78/7xqUNj8ujtY/s1600/image02.png",
+	scores: [
+	"4",
+	"1",
+	"2",
+	"3"
+	]
+},
+{
+	name: "Alex",
+	photo: "https://www.smashingmagazine.com/wp-content/uploads/2015/06/10-dithering-opt.jpg",
+	scores: [
+	"4",
+	"2",
+	"4",
+	"4"
+	]
+}
+
+
+
+]
+
+module.exports = friends;
